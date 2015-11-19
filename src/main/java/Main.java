e188ed8765e4f8d43f1d5aa65b03171c7baa7795
Stas <oalximiko@gmail.com>
2015-11-19 13:46:32 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by USER on 18.11.2015.
+ */
+public class Main {
+    public static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        LOG.debug("sdfghjkl;kjhgfds");
+    }
+}
